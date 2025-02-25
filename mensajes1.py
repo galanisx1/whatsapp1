c7dc3b84cc92161338542d4e1b033f7f213ed523
galanisx1 <100341218+galanisx1@users.noreply.github.com>
2025-02-25 11:43:09 -0600

    Update mensajes1.py

--- mensajes1.py
+++ mensajes1.py
@@ -125,6 +125,6 @@ def webhook():
 
     return jsonify({"status": "Mensaje procesado"}), 200
 
-# Iniciar la aplicación Flask
-if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+# Inicia el servidor en el puerto 5000
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
